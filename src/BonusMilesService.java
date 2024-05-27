3db9187238ee896537814a2d844ae5064139acab
Anastasia Lobanova <lobanovanastasia@mail.ru>
2024-05-27 10:48:55 +0500

    Change code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,13 +1,13 @@
 public class BonusMilesService {
     public int calculate(int cost) {
 
-        int bonusMili = 20;
+        int bonusMiles = 20;
         int price;
         price = cost;
 
         int miles = 0;
-        if (price > bonusMili) {
-            miles = price / bonusMili;
+        if (price >= bonusMiles) {
+            miles = price / bonusMiles;
         }
         return miles;
     }
